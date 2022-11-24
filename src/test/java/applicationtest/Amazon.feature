0be593124ba2985f992917d0f8bf53b2f3f9e5da
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-11-24 21:11:58 +0530

    First commit

---
+++ src/test/java/applicationtest/Amazon.feature
@@ -0,0 +1,5 @@
+Feature: Check the status of different types of order
+Scenario: Check the previously order product
+Given User should be at previously orders page
+When user click on the order
+Then He should be able to see the order details
---
+++ src/test/java/steps/AmazonSteps.java
@@ -0,0 +1,29 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AmazonSteps {
+	
+	
+	@Given("User should be at previously orders page")
+	public void user_should_be_at_previously_orders_page() {
+
+
+	}
+
+	@When("user click on the order")
+	public void user_click_on_the_order() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("He should be able to see the order details")
+	public void he_should_be_able_to_see_the_order_details() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
